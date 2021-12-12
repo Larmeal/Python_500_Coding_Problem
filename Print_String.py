184b357e40c3606b6bd930bacde08c1bcf8554f2
Larmeal <armloveyou252@hotmail.com>
2021-12-12 11:15:55 +0700

    Problem_of_Print_Command

---
+++ Print_String.py
@@ -0,0 +1,59 @@
+#Problem no.1
+print("Hello")
+
+#Problem no.2
+print("Hello world")
+
+#Problem no.3
+print("Hello_python")
+
+#Problem no.4
+print("Merry X'Mas")
+
+#Problem no.5
+print('I want to ask you "why don\'t you drive to work ?"')
+
+#Problem no.6
+print("I don't have a car")
+
+#Problem no.7
+print("You got a new job !? That's so eciting")
+
+#Problem no.8
+print("สวัสดีวันจันทร์")
+
+#Problem no.9
+print("ความแตกต่างระหว่างคนเก่งกับคนไม่เก่ง คือ \"การใช้เวลาว่างให้เป็นประโยชน์\"")
+
+#Problem no.10
+print("/\\/\\/\\")
+
+#Problem no.11
+print("""
+>>> a
+    an
+    ant""")
+
+#Problem no.12
+print("""
+        *
+    *   *   *
+        *""")
+
+#Problem no.13
+print("""
+    *   +   *
+    +   *   +
+    *   +   *""")
+
+#Problem no.14
+print("""
+>>> Just because something
+    thinks differently from you,
+    dose that mean it's not thinking ?""")
+
+#Problem no.15
+print("""
+\\              /
+        X
+/              \\""")
