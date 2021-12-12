dc03b3a59dfd32f939bb9a3a23bcec6294f48ca1
Larmeal <armloveyou252@hotmail.com>
2021-12-12 23:25:15 +0700

    Problem_of_Print_Command

---
+++ Print_Numeric.py
@@ -0,0 +1,20 @@
+#Problem no.16
+print(25)
+
+#Problem no.17
+print("%f" %100)
+
+#Problem no.18
+pi = 22 / 7
+print("%.15f" %float(pi))
+
+from math import pi
+print("%.15f" %pi)
+
+#Problem no. 19
+a = 2
+print("%d" %a)
+
+#Problem no. 20
+a = 12.5
+print("%0.1f" %a)
---
+++ Print_String_Numeric.py
@@ -0,0 +1,48 @@
+#Problem no.21
+a = 2
+b = 3
+print(">>> " + str(a) + " x " + str(b) + " = " + str(a * b))
+
+#Problem no.22
+a = 2
+b = 3
+print(f">>> {a} + {b} = {b} + {a} = {a + b}")
+
+#Problem no.23
+a = 2
+b = 3
+c = 5
+print(f">>> {a}*({b} + {c}) = {a}*{b} + {a}*{c}")
+
+#Problem no.24
+a = 2.4
+b = 2.5
+print(">>> " + str(a) + " + " + str(b) + " = " + str("%0.4f" %(a + b)))
+
+#Problem no.25
+a = 5
+b = 2
+print(">>> " + str("%0.2f" %(a)) + " - " + str("%0.2f" %(b)) + " = " + str("%0.4f" %(a - b)))
+
+#Problem no.26
+birthday = 25
+print("ฉันเกิดวันที่ " + str(birthday) + " ธันวาคม")
+
+#Problem no.27
+a = 5
+b = 100
+print(str(a) + " เท่าของ " + str(100) + " มีค่าเท่ากับ " + str(a * b))
+
+#Problem no.28
+a = 3.5
+print(">>> เขามีเงินเยอะกว่าฉัน " + str(a) + " บาท")
+
+#Problem no.29
+a = 5
+print(">>> ฉันได้กำไร " + str(a) + " %" )
+
+#Problem no.30
+a = 2
+b = 3.5
+print(">>> เมื่อวานฉันขาดทุน " + str(a) + "%" + " วันนี้ฉันได้กำไร " + str(b) + " % ")
+
